cd9813081a028a76c1f8647a04d3b90b4ada21ed
esinda <denis.esin0704@gmail.com>
2026-01-10 15:53:51 +0700

    Add command line arguments parsing

--- src/main/java/shift/filter/ArgsParser.java
+++ src/main/java/shift/filter/ArgsParser.java
@@ -1,4 +1,78 @@
 package shift.filter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArgsParser {
+
+    private boolean append = false;
+    private boolean shortStat = false;
+    private boolean fullStat = false;
+    private String outputPath = ".";
+    private String prefix = "";
+    private final List<String> inputFiles = new ArrayList<>();
+
+    public ArgsParser(String[] args) {
+        parse(args);
+    }
+
+    private void parse(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+            switch (arg) {
+                case "-a" -> append = true;
+                case "-s" -> shortStat = true;
+                case "-f" -> fullStat = true;
+
+                case "-o" -> {
+                    if (i + 1 < args.length) {
+                        outputPath = args[++i];
+                    } else {
+                        System.out.println("Ошибка: после -o должен быть указан путь.");
+                    }
+                }
+
+                case "-p" -> {
+                    if (i + 1 < args.length) {
+                        prefix = args[++i];
+                    } else {
+                        System.out.println("Ошибка: после -p должен быть указан префикс.");
+                    }
+                }
+
+                default -> {
+                    if (arg.startsWith("-")) {
+                        System.out.println("Предупреждение: неизвестная опция " + arg);
+                    } else {
+                        inputFiles.add(arg);
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean isAppend() {
+        return append;
+    }
+
+    public boolean isShortStat() {
+        return shortStat;
+    }
+
+    public boolean isFullStat() {
+        return fullStat;
+    }
+
+    public String getOutputPath() {
+        return outputPath;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public List<String> getInputFiles() {
+        return inputFiles;
+    }
 }
--- src/main/java/shift/filter/Main.java
+++ src/main/java/shift/filter/Main.java
@@ -1,14 +1,29 @@
 package shift.filter;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Утилита запущена.");
-        System.out.println("Получено аргументов: " + args.length);
 
-        for (String arg : args) {
-            System.out.println("Аргумент: " + arg);
+        ArgsParser parser = new ArgsParser(args);
+
+        System.out.println("Append mode: " + parser.isAppend());
+        System.out.println("Output path: " + parser.getOutputPath());
+        System.out.println("Prefix: " + parser.getPrefix());
+
+        if (parser.isFullStat()) {
+            System.out.println("Statistics: FULL");
+        } else if (parser.isShortStat()) {
+            System.out.println("Statistics: SHORT");
+        } else {
+            System.out.println("Statistics: NONE");
         }
 
-        System.out.println("Обработка пока не реализована.");
+        System.out.println("Input files:");
+        for (String file : parser.getInputFiles()) {
+            System.out.println(" - " + file);
+        }
+
+        System.out.println("Обработка данных пока не реализована.");
     }
 }
